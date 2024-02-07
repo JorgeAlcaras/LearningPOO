715b96225bdcdbcead91d4d9803939da3b89ff8c
Jorge Alcaras <jorge.alcaras@cceo.com.mx>
2024-02-07 17:28:28 -0600

    Feature (LearningPOO): 📝The OOP course exercises have finished and the files have been added.

--- Persona_POO.cpp
+++ Persona_POO.cpp
@@ -5,29 +5,30 @@ using namespace std;
 
 class Persona {
 private:
-  static int numeroPersonas;
   string nombre;
   int edad;
 
 public:
-  Persona(string nombre, int edad);
+  static int numeroPersonas;
+  Persona(string nombre, int edad) {
+    this->nombre = nombre;
+    this->edad = edad;
+    numeroPersonas += 1;
+  }
   void setNombre(string nombre) { this->nombre = nombre; }
   void setEdad(int edad) { this->edad = edad; }
-  void saludar();
+  void saludar() {
+    cout << "Hola, soy " << nombre << " y tengo " << edad << " años" << endl;
+  }
 };
 int Persona::numeroPersonas = 0;
-void Persona::saludar() {
-  cout << "Hola, soy " << nombre << " y tengo " << edad << " años" << endl;
-}
-Persona::Persona(string nombre, int edad) {
-  this->nombre = nombre;
-  this->edad = edad;
-}
 
 int main() {
-  Persona *persona1 = new Persona("Jorge", 19);
-  Persona *persona2 = new Persona("Nicolas", 18);
 
-  persona1->saludar();
-  persona2->saludar();
+  Persona persona1 = Persona("Jorge", 19);
+  Persona persona2 = Persona("Nicolas", 18);
+  cout << "Numero de personas: " << Persona::numeroPersonas << endl;
+
+  persona1.saludar();
+  persona2.saludar();
 }

602b6ca5e610bf2e7fd51a335fad3a94f45c90bb
Jorge Alcaras <jorge.alcaras@cceo.com.mx>
2024-02-08 14:54:33 -0600

    BUGFIX (Punteros): 🐛 The content of the punteros file was replaced.

--- Punteros.cpp
+++ Punteros.cpp
@@ -3,31 +3,19 @@
 
 using namespace std;
 
-class Persona {
-private:
-  static int numeroPersonas;
-  string nombre;
-  int edad;
-
-public:
-  Persona(string nombre, int edad);
-  void setNombre(string nombre) { this->nombre = nombre; }
-  void setEdad(int edad) { this->edad = edad; }
-  void saludar();
-};
-int Persona::numeroPersonas = 0;
-void Persona::saludar() {
-  cout << "Hola, soy " << nombre << " y tengo " << edad << " años" << endl;
-}
-Persona::Persona(string nombre, int edad) {
-  this->nombre = nombre;
-  this->edad = edad;
-}
-
 int main() {
-  Persona *persona1 = new Persona("Jorge", 19);
-  Persona *persona2 = new Persona("Nicolas", 18);
-
-  persona1->saludar();
-  persona2->saludar();
+  char character = 'A';
+  char *pointer = &character;
+  string name = "Juan";
+  string *pointer2 = &name;
+  cout << "Character: " << character << endl;
+  cout << "Character int: " << (int *)&character << endl;
+  cout << "Pointer int: " << (int *)pointer << endl;
+  cout << "*Pointer: " << *pointer << endl;
+  cout << "Name: " << name << endl;
+  cout << "Name int: " << (int *)&name << endl;
+  cout << "Pointer2 int: " << (int *)pointer2 << endl;
+  cout << "*Pointer2: " << *pointer2 << endl;
+  cout << "Pointer space: " << &pointer << endl;
+  cout << "Pointer2 space: " << &pointer2 << endl;
 }
